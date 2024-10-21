643d38894930fa3c53b397052baa1e359f8b87d5
Kaze aja <michaelagam03@gmail.com>
2024-10-21 10:39:59 +0800

    Update ytdl.service.js

--- src/services/api/ytdl.service.js
+++ src/services/api/ytdl.service.js
@@ -1,96 +1,113 @@
 const { validations } = require('./../../config/validations.config.js');
 const Response = require('./../../config/response.config.js');
+const { APP_URL, IS_DEV } = require('./../../config/constants.config.js');
 const ytdl = require('@distube/ytdl-core');
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-require('dotenv').config();
 
 /**
  * Ytdl Services
  */
 const YtdlService = {
     response: [],
+
     index: async function (request, response) {
         try {
             // request body validations
-            let { url } = await validations(request, response, { location: 'body' });
+            const { url } = await validations(request, response, { location: 'body' });
 
             // get all video info
-            let info = await ytdl.getInfo(url);
-            let formats = info.formats.filter((format) => format.container === 'mp4' && format.hasVideo === true && format.hasAudio === true);
-            let { title, description, author, video_url, thumbnails } = info.videoDetails;
+            const info = await ytdl.getInfo(url);
+            const formats = info.formats.filter(format => format.container === 'mp4' && format.hasVideo && format.hasAudio);
+            const { title, description, author, video_url, thumbnails } = info.videoDetails;
 
             // add filtered formats to response
             this.response = { title, description, author, video_url, thumbnails, formats };
 
             // return response
-            return response.status(200).json(Response.success(200, this.response, 'Successfully get all available video formats'));
+            return response.status(200).json(Response.success(200, this.response, 'Successfully retrieved all available video formats'));
         } catch (error) {
-            if (request.app.get('env') === 'development') {
-                if (typeof error === 'object') {
-                    return response.status(403).json(Response.error(403, error));
-                } else {
-                    return response.status(500).json(Response.error(500, String(error)));
-                }
-            } else {
-                return response.status(500).json(Response.error(500, 'Server error.'));
-            }
+            return this.handleError(error, response);
         }
     },
+
     store: async function (request, response) {
         try {
             // request body validations
-            let { url } = await validations(request, response, { location: 'body' });
+            const { url } = await validations(request, response, { location: 'body' });
 
             // get video info
-            let info = await ytdl.getInfo(url);
-
-            // add video info to response
-            let { title, description, author, video_url, length_seconds, thumbnails, view_count } = info.videoDetails;
-            this.response = { title, description, author, video_url, length_seconds, thumbnails, view_count };
+            const info = await ytdl.getInfo(url);
+            const { title, description, author, video_url, length_seconds, thumbnails, view_count } = info.videoDetails;
 
             // define file properties
-            let fileName = `${uuidv4()}.mp4`; // Generate a UUID file name
-            let dirname = process.cwd() + '/public/tmp/ytdl/';
-            let filePath = path.join(dirname, fileName);
+            const fileName = `${uuidv4()}.mp4`; // Generate a UUID file name
+            const dirname = path.join(process.cwd(), 'public', 'tmp', 'ytdl');
+            const filePath = path.join(dirname, fileName);
 
-            // add fileUrl to response
-            this.response.fileUrl = `${process.env.APP_HOST}/tmp/ytdl/${fileName}`;
+            // add video info and file URL to response
+            this.response = {
+                title,
+                description,
+                author,
+                video_url,
+                length_seconds,
+                thumbnails,
+                view_count,
+                fileUrl: `${APP_URL}/tmp/ytdl/${fileName}`
+            };
 
             // download video
-            console.log('Downloading video from :', video_url);
+            console.log('Downloading video from:', video_url);
 
             // write stream to file
-            ytdl(video_url, { filter: (format) => format.contentLength })
+            ytdl(video_url, { filter: format => format.contentLength })
                 .pipe(fs.createWriteStream(filePath))
                 .on('finish', () => {
                     console.log('Finished downloading video');
-                    return response.status(200).json(Response.success(200, this.response, 'Successfully download video'));
+                    return response.status(200).json(Response.success(200, this.response, 'Successfully downloaded video'));
                 })
-                .on('error', (err) => {
+                .on('error', err => {
                     console.error(`Error downloading video: ${err}`);
                     return response.status(500).json(Response.error(500, 'Error downloading video'));
                 })
                 .on('close', () => {
                     console.log('Closed downloading video');
 
-                    // make it auto delete after 60 seconds
+                    // auto delete after 60 seconds
                     setTimeout(() => {
-                        fs.unlinkSync(filePath);
-                        console.log('Deleted video file :', filePath);
+                        fs.unlink(filePath, (err) => {
+                            if (err) {
+                                console.error(`Error deleting video file: ${err}`);
+                            } else {
+                                console.log('Deleted video file:', filePath);
+                            }
+                        });
                     }, 60_000);
                 });
         } catch (error) {
-            if (request.app.get('env') === 'development') {
-                if (typeof error === 'object') {
-                    return response.status(403).json(Response.error(403, error));
-                } else {
-                    return response.status(500).json(Response.error(500, String(error)));
-                }
-            } else {
-                return response.status(500).json(Response.error(500, 'Server error.'));
+            return this.handleError(error, response);
+        }
+    },
+
+    handleError: function (error, response) {
+        if (IS_DEV) {
+            if (typeof error === 'object') {
+                return response.status(403).json(Response.error(403, error));
             }
+            return response.status(500).json(Response.error(500, String(error)));
+        }
+        return response.status(500).json(Response.error(500, 'Server error.'));
+    },
+
+    search: async function (request, response) {
+        const { query } = request.query; 
+        try {
+            const results = await ytdl.search(query); 
+            return response.status(200).json(Response.success(200, results, 'Search results'));
+        } catch (error) {
+            return response.status(500).json(Response.error(500, 'Server error.'));
         }
     }
 };
