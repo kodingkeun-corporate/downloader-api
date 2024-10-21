cd90620d7a6ab9e64e478937772471c4b97f017f
Kaze aja <michaelagam03@gmail.com>
2024-10-21 10:41:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const createError = require('http-errors');
 const cors = require('cors');
-
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const indexRouter = require('./src/routes/index.js');
+const { APP_URL, PORT, IS_DEV } = require('./src/config/constants.config.js');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,26 +20,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.set('json spaces', 2);
 
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
+
+// Routes
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// Catch 404 and forward to error handler
+app.use((_, __, next) => {
     next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res) {
-    // set locals, only providing error in development
+// Error handler
+app.use((err, _, res) => {
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = IS_DEV ? err : {};
 
-    // render the error page
+    // Render the error page
     res.status(err.status || 500);
     res.json({ message: err.message });
 });
 
+// Start server
 app.listen(PORT, () => {
-    console.log('SERVER LISTEN ON PORT:', PORT);
+    console.log(`🔥 Server listening on ${APP_URL}`);
 });
 
 module.exports = app;
